c5f2c411c983efe63108eb7cc1194730ee44fea3
Richard Alcaide <richardalcaide16@gmail.com>
2023-09-05 14:42:44 +0800

    afas

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-// Serve static files from the specified directory
-app.use(express.static(path.join('/opt/render/project/src/public')));
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, '/')));
 
 // Catch-all route
 app.get('*', (req, res) => {
-  res.sendFile(path.join('/opt/render/project/src/public', 'index.html'));
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// Enable CORS for all routes or specify origins accordingly
+app.use(cors());
