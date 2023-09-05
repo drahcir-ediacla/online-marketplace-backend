d72aeedd577b61faecfb048017860e0fb219a247
Richard Alcaide <richardalcaide16@gmail.com>
2023-09-05 11:51:07 +0800

    public

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve the frontend build files
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/public')));
 
 // Handle other API routes in the backend
 app.get('/api/someRoute', (req, res) => {
@@ -12,7 +12,7 @@ app.get('/api/someRoute', (req, res) => {
 
 // Always serve the main index.html for any other route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
 });
 
 const port = process.env.PORT || 3001;
