7a4bba8a44017454c19b8d147b0ed58675a3571a
Richard Alcaide <richardalcaide16@gmail.com>
2023-09-05 13:00:35 +0800

    asas

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
 
-// Serve the frontend build files
-app.use(express.static(path.join(__dirname, 'frontend/public')));
+app.get('/', (req, res) => {
+    res.send('Hello!!! World!')
+})
 
-// Handle other API routes in the backend
-app.get('/api/someRoute', (req, res) => {
-  // Handle API logic here
-});
-
-// Always serve the main index.html for any other route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend/public', 'index.html'));
-});
-
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
