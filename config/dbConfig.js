8d12bb8a41fa7b6368d0bb6c1902ae0fda07e22c
drahcir-ediacla <richardalcaide16@gmail.com>
2023-09-11 19:55:28 +0800

    wewfd

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.PORT,
+    port: 3306,
 });
 
 // Check the database connection
