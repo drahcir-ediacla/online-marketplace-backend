3a64e95338e153f0866966d5375aabb921cbef15
drahcir-ediacla <richardalcaide16@gmail.com>
2023-09-24 09:53:36 +0800

    server to

--- server_backup.js
+++ server_backup.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-require("./config/passportConfig");
+require("./controllers/passportController");
 const passport = require("passport")
-const corsOptions = require('./config/corsOptions');
 const app = express();
 app.use(express.json());
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
@@ -16,6 +16,7 @@ const otpRoutes = require('./routes/otpRoutes')
 const passportRoutes = require('./routes/passportRoutes')
 const refreshRoutes = require('./routes/refreshRoutes')
 const credentials = require('./middleware/credentials')
+const sessionStore = require('./config/sessionsConfig')
 
 const port = process.env.PORT || 8081;
 
@@ -24,18 +25,38 @@ const port = process.env.PORT || 8081;
 // custom middleware logger
 app.use(logger);
 
+// Cross Origin Resource Sharing
+app.use(cors({
+  origin: ["http://localhost:3000", "https://yogeek.onrender.com"],
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
+// Middleware to set the required header
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Private-Network', 'true');
+  next();
+});
+
 // Handle options credentials check - before CORS!
 // and fetch cookies credentials requirement
 app.use(credentials);
 
-// Cross Origin Resource Sharing
-app.use(cors());
-
 
 app.use(bodyParser.json());
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: sessionStore,
+    cookie: { secure: false, maxAge: 3600000 },
+  }));
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 
 
 // built-in middleware to handle urlencoded data
