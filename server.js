478ece3b014fc381f66c11245bff730130bc23f5
Richard Alcaide <richardalcaide16@gmail.com>
2023-09-28 09:58:02 +0800

    sdsd

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
-    cookie: { httpOnly: false, sameSite: 'lax', secure: false, maxAge: 3600000 },
+    cookie: { httpOnly: true, sameSite: 'lax', secure: true, maxAge: 3600000 },
   }));
 
 app.use(passport.initialize());
