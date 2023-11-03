04423d8ae087289c22c910ed216334ea5e15baca
drahcir-ediacla <richardalcaide16@gmail.com>
2023-11-03 22:16:37 +0800

    google

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     store: sessionStore,
-    cookie: { httpOnly: true, sameSite:none, secure: true, maxAge: 3600000 },
+    cookie: { httpOnly: true, secure: false, maxAge: 3600000 },
     proxy: true,
   })
 );
