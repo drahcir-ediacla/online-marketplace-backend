bb1eaa70908068647113edddec45d7d3e5ba509f
Richard Alcaide <richardalcaide16@gmail.com>
2023-12-21 14:00:42 +0800

    mostviewed routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -21,6 +21,6 @@ router.get('/api/getuserwishlist/:user_id', ProductController.getWishlistByUserI
 
 // Product view routes
 router.post('/api/product/view/:id', ProductController.addProductView);
-router.get('/api/product/mostview', ProductController.findMostViewedProducts);
+router.get('/api/product/most-viewed', ProductController.findMostViewedProducts);
 
 module.exports = router;
