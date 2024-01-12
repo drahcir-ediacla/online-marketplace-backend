8e9237e46441d128410911a2f275dcc06730c739
Richard Alcaide <richardalcaide16@gmail.com>
2024-01-12 14:25:10 +0800

    socket

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -1,5 +1,5 @@
 const socketIo = require('socket.io');
-require('dotenv').config()
+require('dotenv').config();
 
 const configureSocket = (server) => {
   const io = socketIo(server, {
@@ -12,8 +12,21 @@ const configureSocket = (server) => {
   io.on('connection', (socket) => {
     console.log('New client connected');
 
+    // Join chat room based on chat_id
+    socket.on('joinChat', (chatId) => {
+      socket.join(chatId);  // Join the room based on chat_id
+    });
+
+    // Handle sending messages to a specific chat room (chat_id)
     socket.on('send_message', (data) => {
-      io.emit('receive_message', data); // Broadcast message to all connected clients
+      // Ensure data contains chat_id and message content
+      if (data.chat_id && data.content) {
+        // Emit message to the specific chat room (chat_id)
+        io.to(data.chat_id).emit('receive_message', data);
+      } else {
+        // Handle error if chat_id or content is missing
+        console.error('Invalid data received for sending message:', data);
+      }
     });
 
     socket.on('disconnect', () => {
