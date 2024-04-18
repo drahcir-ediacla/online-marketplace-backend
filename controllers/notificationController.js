8ada27cbb482d6c5293e2aa76c4b9fdc261d7d54
Richard Alcaide <richardalcaide16@gmail.com>
2024-04-18 17:04:40 +0800

    API notification

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,10 +1,10 @@
 const { notificationModel } = require('../config/sequelizeConfig')
 
 
-const getNotificationsById = async (req, res) => {
+const getNotificationsByUserId = async (req, res) => {
     try {
         if (!req.isAuthenticated()) {
-            return res.status(401).json({ error: 'Authentication is required to view notifications.' })
+            return res.status(401).json({ error: 'Authentication is required to fetch notifications.' })
         }
 
         const userId = req.user.id;
@@ -22,4 +22,40 @@ const getNotificationsById = async (req, res) => {
     }
 }
 
-module.exports = {getNotificationsById}
+
+
+const readNotification = async (req, res) => {
+    try {
+        // Check if the user is authenticated
+        if (!req.isAuthenticated()) {
+          return res.status(401).json({ error: 'Authentication is required to mark notifications as read.' });
+        }
+    
+        const userId = req.user.id;
+        const notificationId = req.params.notificationId;
+    
+        // Find the notification by ID and ensure it belongs to the authenticated user
+        const userNotification = await notificationModel.findOne({
+          where: {
+            id: notificationId,
+            recipient_id: userId,
+          },
+        });
+    
+        // If the notification doesn't exist or doesn't belong to the user, return 404
+        if (!userNotification) {
+          return res.status(404).json({ error: 'Notification not found.' });
+        }
+    
+        // Update the 'read' status of the notification
+        await userNotification.update({ read: true });
+    
+        // Return a success message
+        res.status(200).json({ message: 'Notification marked as read successfully.' });
+      } catch (error) {
+        console.error('Error marking notification as read:', error);
+        res.status(500).json({ error: 'An error occurred while marking notification as read.' });
+      }
+}
+
+module.exports = {getNotificationsByUserId, readNotification}
--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router();
 const NotificationController = require('../controllers/notificationController')
 
-router.get('/api/notifications', NotificationController.getNotificationsById);
+router.get('/api/notifications', NotificationController.getNotificationsByUserId);
+router.put('/api/read-notifications/:notificationId', NotificationController.readNotification)
 
 
 module.exports = router;
