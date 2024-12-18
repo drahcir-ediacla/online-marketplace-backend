8ed89c6be0ecd3bb95d03878486cf4853254b842
Richard Alcaide <richardalcaide16@gmail.com>
2024-12-18 20:41:31 +0800

    DBConfig

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 100,
     queueLimit: 10,
-    port: 3306,
+    port: process.env.DB_PORT,
 });
 
 // Check the database connection
