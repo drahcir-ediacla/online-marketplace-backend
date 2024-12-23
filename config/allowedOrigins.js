1f3b1a7309142241ddd4c061d7774213bf882e3e
Richard Alcaide <firstg.bpo@gmail.com>
2024-12-23 16:26:04 +0800

    126 be

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -6,7 +6,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3000',
     'http://localhost:8081',
-    'https://cytv125.com'
+    'https://cytv126.com'
 ];
 
 module.exports = allowedOrigins;
