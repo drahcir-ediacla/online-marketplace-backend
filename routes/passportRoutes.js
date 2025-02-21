d4fe59e7ca82aff2f3b100441414533baf779c44
Richard Alcaide <richardalcaide16@gmail.com>
2025-02-21 18:48:18 +0800

    lazy commit

--- routes/passportRoutes.js
+++ routes/passportRoutes.js
@@ -96,9 +96,9 @@ router.get(
     io.emit('updateUserStatus', { id: userId, status: 'online' });
 
     // Set the refresh token and access token as cookies
-    res.cookie('refreshJWT', refreshToken, { httpOnly: true, secure: false, maxAge: 24 * 60 * 60 * 1000, path: '/' });
-    res.cookie('jwt', accessToken, { httpOnly: true, secure: false, maxAge: 15 * 60 * 1000, path: '/' });  //maxAge is equivalent to 24 hours
-    console.log('jwt:', accessToken)
+    res.cookie('refreshJWT', refreshToken, { httpOnly: true, sameSite: 'none', secure: true, maxAge: 24 * 60 * 60 * 1000, path: '/' });
+    // res.cookie('jwt', accessToken, { httpOnly: true, secure: false, maxAge: 15 * 60 * 1000, path: '/' });  //maxAge is equivalent to 24 hours
+    // console.log('jwt:', accessToken)
 
     res.redirect(`${process.env.CLIENT_URL}/auth/success`);  // Redirect to the desired page after successful login
   }
